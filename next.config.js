a995f8e242e4bde46c15533d1e26848ea7e851fc
Nelson Rosales <nelsonrosales@gmail.com>
2025-02-09 23:51:42 -0500

    Quiz app ready for deployment

--- next.config.js
+++ next.config.js
@@ -1,11 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
-  output: 'standalone',
-  experimental: {
-    appDir: true,
-  }
-};
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
